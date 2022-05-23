8ef86ba6a36477dd19b913de98c03c23644daa29
mona <bushra.kiran108@gmail.com>
2022-05-24 03:28:44 +0500

    Create Stories_content.js

---
+++ src/Components/Stories_content.js
@@ -0,0 +1,42 @@
+
+import { Grid, Typography } from '@mui/material';
+import * as React from 'react';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
+import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
+
+function Story_bar() {
+    return (
+  <>
+   <div className='Story_bar'
+    style={{padding:"2%"}} >
+         
+    <img style={{  width:"120px", height:"200px"}} src="https://k.nooncdn.com/cms/pages/20220505/373fee90706065bcef842dff3cb58c6b/en_mb_uae-cat-grocery-01.png" alt="https://daily.noon.com" class="sc-egiyK cwhUbk"></img>
+    <img style={{  width:"120px", height:"200px"}} src="https://k.nooncdn.com/cms/pages/20220505/373fee90706065bcef842dff3cb58c6b/en_mb_uae-cat-17.png" alt="/outlet-store-21-ae" class="sc-egiyK cwhUbk"></img>
+    <img style={{  width:"120px", height:"200px"}} src="https://k.nooncdn.com/cms/pages/20220505/373fee90706065bcef842dff3cb58c6b/en_mb_uae-cat-19.png" alt="/gift-card" class="sc-egiyK cwhUbk"></img>
+    <img style={{  width:"120px", height:"200px"}} src="https://k.nooncdn.com/cms/pages/20220519/14758f75b4b4ea21bd847e526bad57f5/en_mb_uae-cat-04.png" alt="/toys" class="sc-egiyK cwhUbk"></img>
+    <img style={{  width:"120px", height:"200px"}} src="https://k.nooncdn.com/cms/pages/20220520/beacb7e5e2534b99145cf1f67b545e8e/en_mb_uae-cat-04.png" alt="/baby" class="sc-egiyK cwhUbk"></img>
+    <img style={{  width:"120px", height:"200px"}} src="https://k.nooncdn.com/cms/pages/20220505/373fee90706065bcef842dff3cb58c6b/en_mb_uae-cat-01.png" alt="/mobiles" class="sc-egiyK cwhUbk"></img>
+    <img style={{  width:"120px", height:"200px"}} src="https://k.nooncdn.com/cms/pages/20220505/373fee90706065bcef842dff3cb58c6b/en_mb_uae-cat-23.png" alt="/laptops" class="sc-egiyK cwhUbk"></img>
+    <img style={{  width:"120px", height:"200px"}} src="https://k.nooncdn.com/cms/pages/20220505/373fee90706065bcef842dff3cb58c6b/en_mb_uae-cat-27.png" alt="/gaminghub" class="sc-egiyK cwhUbk"></img>
+    <img style={{  width:"120px", height:"200px"}} src="https://k.nooncdn.com/cms/pages/20220520/beacb7e5e2534b99145cf1f67b545e8e/en_mb_uae-cat-01.png" alt="/beauty" class="sc-egiyK cwhUbk"></img>
+    <img style={{  width:"120px", height:"200px"}} src="https://k.nooncdn.com/cms/pages/20220505/373fee90706065bcef842dff3cb58c6b/en_mb_uae-cat-02.png" alt="/home-kitchen" class="sc-egiyK cwhUbk"></img>
+    <img style={{  width:"120px", height:"200px"}} src="https://k.nooncdn.com/cms/pages/20220505/373fee90706065bcef842dff3cb58c6b/en_mb_uae-cat-05.png" alt="/audio-video" class="sc-egiyK cwhUbk"></img>
+    <img style={{  width:"120px", height:"200px"}} src="https://k.nooncdn.com/cms/pages/20220505/373fee90706065bcef842dff3cb58c6b/en_mb_uae-cat-10.png" alt="/home-appliances" class="sc-egiyK cwhUbk"></img>
+    
+  </div>
+
+  </>
+
+    );
+}
+function Arrow_slider() {
+  return (
+<>
+<div className='Arrow_slider'>
+  <ArrowCircleLeftIcon    style={{float:"left",marginTop:"-10.86%",  marginRight: "96.12%"}} />
+  <ArrowCircleRightIcon   style={{float:"left",marginTop:"-10.86%",  marginLeft: "96.12%" }} /></div>
+</>
+);
+}
+export { Story_bar , Arrow_slider };
